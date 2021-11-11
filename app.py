e391f9a96df6ef594b95eb3a38762a181d6526c4
JoeyDeRosa <joeyderosa11.jd@gmail.com>
2021-11-11 10:17:43 -0800

    created a rough outline

---
+++ app.py
@@ -0,0 +1,27 @@
+# outline for file hasher
+
+def run_app
+  input =
+  folders = [input]
+  if input == folder:
+    for i in folders:
+      if i == file:
+        if hash == True:
+          hash(file)
+        else:
+          un_hash(file):
+      if i == folder:
+        folders.append(i)
+  else:
+    if hash == True:
+      hash(file)
+    else:
+      un_hash(file)
+
+def hash
+  for i in file:
+    i = hash_key
+
+def un_hash
+  for i in file:
+    i = un_hash_key
